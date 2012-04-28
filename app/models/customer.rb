df57fcda27f4aa2d2bbf730b0eabe09193a1d2fd
emadb <emanuele@codiceplastico.com>
2012-04-29 00:05:06 +0200

    infrastructure and some test functionality

---
+++ app/models/customer.rb
@@ -0,0 +1,11 @@
+class Customer
+  include Mongoid::Document
+  field :name, :type => String
+  field :vat_code, :type => String
+  
+  field :email, :type => String
+  embeds_one :address
+  accepts_nested_attributes_for :address
+  
+  delegate :street, :to => :address
+end
---
+++ spec/controllers/invoice_controller_spec.rb
@@ -0,0 +1,5 @@
+require 'spec_helper'
+
+describe InvoiceController do
+
+end
