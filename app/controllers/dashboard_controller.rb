15cc81bdbaae549dcf78a90db415488161db4c3d
emadb <emanuele@codiceplastico.com>
2012-06-12 00:16:18 +0200

    more dashboard functionality

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -5,6 +5,14 @@ class DashboardController < ApplicationController
     invoices.each{ |x| @to_receive = @to_receive + x.total unless x.is_payed }
     @invoices_totals = InvoiceTotalsInfo.new(invoices.sum(:taxable_income), invoices.sum(:tax), invoices.sum(:total))
 
+    @quarters = Quarters.new
+    invoices.each do |i|
+      @quarters.q1  = @quarters.q1 + i.taxable_income if [1,2,3].include?(i.date.month)
+      @quarters.q2  = @quarters.q2 + i.taxable_income if [4,5,6].include?(i.date.month)
+      @quarters.q3  = @quarters.q3 + i.taxable_income if [7,8,9].include?(i.date.month)
+      @quarters.q4  = @quarters.q4 + i.taxable_income if [10, 11, 12].include?(i.date.month)
+    end
+
     inbound_invoices = InboundInvoice.all
     @inbound_invoices_totals = InvoiceTotalsInfo.new(inbound_invoices.sum(:taxable_income), inbound_invoices.sum(:tax), inbound_invoices.sum(:total))
   end
