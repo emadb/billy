cd4b7252326dd30bdf328445c2b42093cae98148
ema <emanuele@codiceplastico.com>
2012-12-27 16:37:23 +0100

    added labels to invoice list

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,24 @@
 module ApplicationHelper
+  def status_helper (status)
+  	case status
+  		when 1 
+  			label_class = ''
+  			text = 'default'
+  		when 2
+  			label_class = 'success'
+  			text = 'success'
+  		when 3
+  			label_class = 'warning'
+  			text = 'warning'
+  		when 4
+  			label_class = 'info'
+  			text = 'info'
+  		when 5
+  			label_class = 'inverse'
+  			text = 'inverse'
+  	end
+  	
+  	raw("<span class=\"label label-#{label_class}\">#{text}</span>")
+  end
+
 end
