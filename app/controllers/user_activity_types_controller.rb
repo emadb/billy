e92bf565b90b6f6db288ede3d317edbe3c34ed07
Emanuele DelBono <emanuele@codiceplastico.com>
2012-12-31 16:20:18 +0100

    bug fixing

--- app/controllers/user_activity_types_controller.rb
+++ app/controllers/user_activity_types_controller.rb
@@ -1,7 +1,7 @@
 class UserActivityTypesController < ApplicationController
   before_filter :user_is_admin?
   def index
-    @user_activities_type = UserActivityType.all.order_by('date')
+    @user_activities_type = UserActivityType.all
     
     respond_to do |format|
       format.json { render :json => @user_activities_type }
