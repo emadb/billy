5cc35e53d2f26c8cb8b01920ba3a900655d0dd4c
Emanuele DelBono <emanuele@codiceplastico.com>
2013-01-02 22:25:24 +0100

    added font awesome support

--- config/application.rb
+++ config/application.rb
@@ -56,7 +56,8 @@ module Scrooge
     # Enable the asset pipeline
     config.assets.enabled = true
     config.assets.initialize_on_precompile = false
-
+    config.assets.paths << Rails.root.join("app", "assets", "fonts")
+    
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
   end
