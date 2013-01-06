d2d0313f91f7abac50b0b5670137c1bd91fe9bef
Emanuele DelBono <emanuele@codiceplastico.com>
2013-01-06 15:42:00 +0100

    default order for customer

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,5 @@
 class Customer < ActiveRecord::Base
+  default_scope order(:name)
 	attr_accessible :address_city, :address_province, :address_street, 
     :address_zip_code, :email, :name, :vat_code,
     :file_name_template
