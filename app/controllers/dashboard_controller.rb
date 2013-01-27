144ad1b2e4d15d2667e418e144ba7004bc297c63
Emanuele DelBono <emanuele@codiceplastico.com>
2013-01-27 10:28:09 +0100

    added month activities on dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -46,7 +46,33 @@ class DashboardController < ApplicationController
   end
 
   def activities
-    UserActivity.select('user_id, hours').where('date >= ? and date < ?', Date.new(2013,1,1), Date.new(2013,2,1)).group('user_id').sum('hours')
+    month = params['month'].to_i 
+    year = params['year'].to_i
+
+    from_date = Date.new(year, month, 1)
+    to_date = from_date + 1.month
+
+    @filter_date = from_date
+    
+    @activities = ActiveRecord::Base.connection.select_all("SELECT users.name, job_orders.code, sum(user_activities.hours) as total 
+      FROM user_activities 
+      INNER JOIN users ON users.id = user_activities.user_id
+      INNER JOIN job_order_activities ON user_activities.job_order_activity_id = job_order_activities.id
+      INNER JOIN job_orders on job_order_activities.job_order_id = job_orders.id
+      WHERE user_activities.date >= '#{from_date.strftime('%Y-%m-%d')}' and user_activities.date <= '#{to_date.strftime('%Y-%m-%d')}'
+      GROUP BY job_orders.id
+      ORDER BY job_orders.code, users.name")
+
+    # @activities = UserActivity
+    #   .joins(:user)
+    #   .select('users.name, sum(user_activities.hours) as total')
+    #   .where('date >= ? and date < ?', Date.new(2013,1,1), Date.new(2013,2,1))
     render :layout=> false
   end
 end
+
+
+
+
+
+
