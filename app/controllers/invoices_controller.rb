b3f8f4127592004f7401676598eb1df9074f0b31
Emanuele DelBono <emanuele@codiceplastico.com>
2013-04-10 18:18:45 +0200

    disabled db

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -67,7 +67,7 @@ class InvoicesController < ApplicationController
     
     if !ENV['DROPBOX_FOLDER']
       drop_box = DropBoxService.new
-      drop_box.upload file_name, full_path
+      #drop_box.upload file_name, full_path
     end
 
     send_file full_path, :type=> "application/pdf", :disposition => 'inline'
