35ab7470d5c2dbe2b56861bb1302690ce2a1ae39
Emanuele DelBono <emanuele@codiceplastico.com>
2013-04-10 21:48:27 +0200

    try again with db

--- app/services/drop_box_service.rb
+++ app/services/drop_box_service.rb
@@ -1,7 +1,7 @@
 class DropBoxService
-
   def upload file_name, file
     client = Dropbox::API::Client.new(:token  => ENV['DROPBOX_TOKEN'], :secret => ENV['DROPBOX_SECRET'])
-    client.upload Scrooge::Application.config.invoice_folder + file_name, File.read(file)
+    file_content = File.read(file)
+    client.upload Scrooge::Application.config.invoice_folder + file_name, file_content
   end
 end
