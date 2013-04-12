61f5e0242020c03e75c40cab92693f92d48e9e9d
Emanuele DelBono <emanuele@codiceplastico.com>
2013-04-12 12:59:36 +0200

    removed unused code

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -3,29 +3,5 @@ class TestController < ApplicationController
 
   def index
     
-    @invoice = Invoice.find(17)
-    file_name = "#{@invoice.number} - #{@invoice.customer.file_name_template}.pdf"
-    full_path = Rails.root.join('tmp', file_name)
-  
-    pdf = render_to_string(
-            :pdf => file_name,
-            :template => 'invoices/show.pdf.html.erb',
-            :margin => { :bottom => 15 },
-            :footer => {
-              :content => '<div class="container" style="text-align:center;color:#777777;font-size:12px;font-family: Helvetica Neue,Helvetica"><p><strong>CodicePlastico srl</strong> - www.codiceplastico.com - Tel: +39 030 6595 241</br>P.IVA, CF e Registro Imprese di Brescia 03079830984 Capitale Sociale : Euro 10.000,00 i.v.</p></div>'
-            })
-
-    File.open(full_path, 'wb') do |file|
-      file.write(pdf)
-    end
-
-    client = Dropbox::API::Client.new(:token  => ENV['DROPBOX_TOKEN'], :secret => ENV['DROPBOX_SECRET'])
-    
-    file_content = File.read(full_path)
-    
-    #client.upload 'prova1.pdf', file_content
-    client.upload 'prova2.pdf', pdf
-
-    send_file full_path, :type=> "application/pdf", :disposition => 'inline'
   end
 end
