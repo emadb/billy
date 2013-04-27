7bd31f67c1b77369a9eb9451b22dbc901c066849
Emanuele DelBono <emanuele@codiceplastico.com>
2013-04-27 11:17:28 +0200

    adding specs

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -11,10 +11,10 @@ describe Invoice do
       expect(@invoice.invoice_items.size).to eq(2)
     end
     it "customer should not be nil" do
-      @invoice.customer.should_not be_nil
+      expect(@invoice.customer).not_to be_nil
     end
     it "number should be nil" do
-      @invoice.number.should be_nil
+      expect(@invoice.number).to be_nil
     end
   end
   
@@ -28,6 +28,35 @@ describe Invoice do
       invoice = Invoice.create_new
       invoice.activate
       expect(invoice.status).to eq(Invoice.active)
+    end
+    it 'should set the correct date and due_date' do
+      invoice = Invoice.create_new
+      date = Date.new(Date.today.year, Date.today.month, 1) - 1
+      invoice.activate
+      expect(invoice.date).to eq(date.end_of_month)
+      expect(invoice.due_date).to eq(date.end_of_month + 30)
+    end
+  end
+
+  context 'is in late' do
+    it 'should not be in late if the due_date has to came' do
+      #invoice = invoices(:invoice1)
+      invoice = Invoice.create_new
+      invoice.due_date = DateTime.now + 10
+      expect(invoice.is_in_late?).to be_false
+    end
+
+    it 'should be in late if the due_date has passed' do
+      invoice = Invoice.create_new
+      invoice.due_date = DateTime.now - 10
+      expect(invoice.is_in_late?).to be_true
+    end
+
+    it 'should not be in late if the due_date has passed but is payed' do
+      invoice = Invoice.create_new
+      invoice.due_date = DateTime.now - 10
+      invoice.is_payed = true
+      expect(invoice.is_in_late?).to be_false
     end
   end
 end
