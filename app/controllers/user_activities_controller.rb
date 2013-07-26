3b83da0bbf00a4da00b3e04c05a656c2e4a74a51
Emanuele DelBono <emanuele@codiceplastico.com>
2013-07-26 16:00:47 +0200

    refactoring

--- app/controllers/user_activities_controller.rb
+++ app/controllers/user_activities_controller.rb
@@ -34,29 +34,43 @@ class UserActivitiesController < ApplicationController
   end
 
   def create
-    @activity = UserActivity.new()
+    activity = UserActivity.new
     
-    @activity.date = DateTime.parse(params[:date])
-    @activity.hours = params[:hours].to_f
-    @activity.description = params[:description]
-    job_order = JobOrder.find(params[:jobOrder])
-    @activity.job_order_activity = job_order.activities.select{|a| a.id = params[:activity]}[0]
-    @activity.user = current_user
-    @activity.save
+    create_or_update activity
+    create_response activity
 
+  end
+
+  def update
+    activity = UserActivity.find(params[:id])
+    create_or_update activity
+    create_response activity
+  end
+
+  def create_response(activity)
     @result = {
-      :id => @activity.id,
-      :date => @activity.date,
-      :hours => @activity.hours,
-      :description => @activity.description,
+      :id => activity.id,
+      :date => activity.date,
+      :hours => activity.hours,
+      :description => activity.description,
       :jobOrder => job_order.code,
-      :activity => JobOrderActivity.find(@activity.job_order_activity.id).description
+      :activity => JobOrderActivity.find(activity.job_order_activity.id).description
     }
 
     respond_to do |format|
       format.html { render :json =>  @result }
       format.json { render :json =>  @result }
-    end      
+    end
+  end
+
+  def create_or_update(activity)
+    activity.date = DateTime.parse(params[:date])
+    activity.hours = params[:hours].to_f
+    activity.description = params[:description]
+    job_order = JobOrder.find(params[:jobOrder])
+    activity.job_order_activity = job_order.activities.select{|a| a.id = params[:activity]}[0]
+    activity.user = current_user
+    activity.save
   end
 
   def destroy
