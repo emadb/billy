819778d8e0cc21684f33b0c5bc19c7ad650d8ae0
Emanuele DelBono <emanuele@codiceplastico.com>
2013-09-09 21:24:36 +0200

    trying new fix for missing fonts
    trying new fix for missing fonts

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,9 @@ module Scrooge
     # config.i18n.default_locale = :de
     config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :it
+    config.assets.enabled = true
     config.assets.paths << Rails.root.join("app", "assets", "font")
+    # Precompile additional assets
+    config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
   end
 end
