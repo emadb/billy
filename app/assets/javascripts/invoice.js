6bfa47c6c7f6cb8062d970afb4bec663ec4e1bc7
Emanuele DelBono <emanuele@codiceplastico.com>
2013-11-05 16:49:10 +0100

    fixed a javascript error

--- app/assets/javascripts/invoice.js
+++ app/assets/javascripts/invoice.js
@@ -1,5 +1,6 @@
 $(function (){
   var iva = $('#iva').val();
+  iva = parseFloat(iva);
   function updateTotals(){
     var total = 0;
     $('.amount:visible').each(function(i, a){
@@ -10,13 +11,14 @@ $(function (){
     });
 
     $('#taxable-income').text(total.toFixed(2));
+
     var hasTax = $('#invoice_has_tax').attr('checked')?true:false;
     if (hasTax){
       $('#tax').text((total * iva).toFixed(2));
-      $('#total').text(((total * (1+ iva))).toFixed(2));
+      $('#total').text(((total * (1 + iva))).toFixed(2));
     } else {
         $('#tax').text(0);
-        $('#total').text((total).toFixed(2));
+        $('#total').text(total.toFixed(2));
     }
   }
 
