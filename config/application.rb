d101e9a9c8c692d6f1b8f808bef6cba3f4f85cd1
Emanuele DelBono <emanuele@codiceplastico.com>
2013-11-15 16:41:14 +0100

    problems with deploy

--- config/application.rb
+++ config/application.rb
@@ -26,5 +26,6 @@ module Scrooge
     # Precompile additional assets
     config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
     config.active_record.whitelist_attributes = false
+    config.assets.initialize_on_precompile = false
   end
 end
