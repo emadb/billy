418e65523139e15f3bf4113df61bcbb5bf28d9af
Emanuele DelBono <emanuele@codiceplastico.com>
2013-12-03 18:26:16 +0100

    fixed dropbox issue

--- app/services/drop_box_service.rb
+++ app/services/drop_box_service.rb
@@ -1,5 +1,10 @@
 class DropBoxService
   def upload file_name, file
+
+    Dropbox::API::Config.app_key = AppSettings.dropbox_app_key
+    Dropbox::API::Config.app_secret = AppSettings.dropbox_app_secret 
+    Dropbox::API::Config.mode = AppSettings.dropbox_app_mode
+
     client = Dropbox::API::Client.new(:token  => AppSettings.dropbox_token, :secret => AppSettings.dropbox_secret)
     client.upload AppSettings.dropbox_folder + file_name, file
   end
