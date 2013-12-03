6eaa293c5986de38d799b8972c0013918a3d319f
Emanuele DelBono <emanuele@codiceplastico.com>
2013-12-03 18:29:24 +0100

    fixed dropbox issue

--- app/services/drop_box_service.rb
+++ app/services/drop_box_service.rb
@@ -1,6 +1,7 @@
 class DropBoxService
   def upload file_name, file
 
+    # TODO: This config should not be here. Move to the initializer.
     Dropbox::API::Config.app_key = AppSettings.dropbox_app_key
     Dropbox::API::Config.app_secret = AppSettings.dropbox_app_secret 
     Dropbox::API::Config.mode = AppSettings.dropbox_app_mode
