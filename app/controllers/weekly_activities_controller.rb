7019ae808848ef93f798cc8660dbed7908a513f7
Emanuele DelBono <emanuele@codiceplastico.com>
2014-11-29 08:01:15 +0100

    completed server side insertion

--- app/controllers/weekly_activities_controller.rb
+++ app/controllers/weekly_activities_controller.rb
@@ -2,9 +2,25 @@ class WeeklyActivitiesController < ApplicationController
 
   def index
     @week = DateTime.now.all_week
+    # monday = DateTime.now.beginning_of_week
+    # activities = UserActivity.
+    #   .where('date >= ? and date <= ? and user_id = ?', monday, monday + 7, current_user.id)
   end
 
   def create
+    activites = params[:weekly_activity][:_json]
+    activites.each do |a|
+      7.times do |d|
+        act = UserActivity.new 
+        act.user_activity_type_id = UserActivityType.working_id
+        act.job_order_activity_id = a[:activity_id]
+        act.user = current_user
+        act.date = DateTime.now.beginning_of_week + d
+        act.hours = a[:hours][d]        
+        act.save
+      end
+    end
+    redirect_to '/user_activites'
   end
 
 end
